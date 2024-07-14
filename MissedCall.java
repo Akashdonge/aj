c560a2ce8352426c251a84ba1483673b84c8c108
Akash donge <114501810+Akashdonge@users.noreply.github.com>
2024-07-15 00:06:15 +0530

    Add files via upload

---
+++ MissedCall.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class MissedCall {
+long number;
+Date date;
+public MissedCall(long number,Date date) {
+this.number=number;
+this.date=date;
+}
+public String toString() {
+return number+" "+date;}}
